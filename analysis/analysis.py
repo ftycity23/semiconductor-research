982abed9e35f9af6fcfdd7fa44e57e20b6c08629
wfrae <felicitytyler49@gmail.com>
2026-07-22 20:16:31 -0400

    Loaded and analyzed

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,14 +1,22 @@
+#imports
+
 import pandas as pd
 import os
 import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 # Loading data
 df_secom = pd.read_csv('datasets/uci-secom.csv')
 
-print(df.head())
+cset_url = 'https://raw.githubusercontent.com/georgetown-cset/eto-chip-explorer/main/data/inputs.csv'
+df_cset = pd.read_csv(cset_url)
+
+print(df_cset.head())
 
 #Analyzizng first data set (uci-secom)
 
@@ -28,3 +36,14 @@ print(df_secom.dtypes.value_counts())
 #first few rows
 
 print(df_secom.head())
+
+#missing values
+
+missing = df_secomd.insull().sum
+
+if missing.sum() > 0:
+    print(f" Total missing values:"{missing.sum()})
+    print(f"   Columns with missing data:")
+    print(missing[missing > 0])
+else:
+    print(" No missing data!")
--- analysis/download_data.py
+++ analysis/download_data.py
@@ -1,6 +1,14 @@
 import kagglehub
 import pandas as pd
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# Then your code
+inputs_url = 'https://raw.githubusercontent.com/georgetown-cset/eto-chip-explorer/main/data/inputs.csv'
+df_inputs = pd.read_csv(inputs_url)
+
 # SECOM data
 path = kagglehub.dataset_download("paresh2047/uci-semcom")
 print(f"Dataset downloaded to: {path}")
@@ -9,7 +17,7 @@ print(f"Dataset downloaded to: {path}")
 # CSET dat
 print("Loading CSET Supply Chain Data...\n")
 
-# Load all the supply chain CSV files
+# Load all the supply chain CSV files w/ exception error
 try:
     inputs_url = 'https://raw.githubusercontent.com/georgetown-cset/eto-chip-explorer/main/data/inputs.csv'
     providers_url = 'https://raw.githubusercontent.com/georgetown-cset/eto-chip-explorer/main/data/providers.csv'
@@ -20,25 +28,6 @@ try:
     df_providers = pd.read_csv(providers_url)
     df_provision = pd.read_csv(provision_url)
     df_stages = pd.read_csv(stages_url)
-    
-    print("✅ INPUTS (tools, materials, processes):")
-    print(f"   Shape: {df_inputs.shape}")
-    print(f"   Columns: {df_inputs.columns.tolist()}\n")
-    
-    print("✅ PROVIDERS (countries, companies):")
-    print(f"   Shape: {df_providers.shape}")
-    print(f"   Columns: {df_providers.columns.tolist()}\n")
-    
-    print("✅ PROVISION (who provides what):")
-    print(f"   Shape: {df_provision.shape}")
-    print(f"   Columns: {df_provision.columns.tolist()}\n")
-    
-    print("✅ STAGES (production stages):")
-    print(f"   Shape: {df_stages.shape}")
-    print(f"   Columns: {df_stages.columns.tolist()}\n")
-    
-    print("\n📊 Sample data from PROVIDERS:")
-    print(df_providers.head())
     
 except Exception as e:
     print(f"❌ Error: {e}")
